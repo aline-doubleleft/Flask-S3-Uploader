7574ab77d8f6c8e361ff5e84bf646d82241adc43
Aline Freitas <a.freitas@doubleleft.com>
2014-01-20 19:12:54 -0200

    working example for upload from s3

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, flash
-from flask.ext.wtf import FileField, Form
+from flask import Flask, render_template, flash, request
+from wtforms import FileField, Form
 from tools import s3_upload
 
 app = Flask(__name__)
@@ -11,10 +11,10 @@ class UploadForm(Form):
 @app.route('/',methods=['POST','GET'])
 def upload_page():
     form = UploadForm()
-    if form.validate_on_submit():
+    if request.method == 'POST' and form.validate():
         output = s3_upload(form.example)
         flash('{src} uploaded to S3 as {dst}'.format(src=form.example.data.filename, dst=output))
     return render_template('example.html',form=form)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host = '0.0.0.0')
